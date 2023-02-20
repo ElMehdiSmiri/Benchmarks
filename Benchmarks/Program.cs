5dde04cdd1e542caa72fff3ea05f6a8dcfb19992
El Mehdi Smiri <ElMehdi.Smiri@dcsl.com>
2023-02-20 22:08:35 +0100

    Add project files.

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,8 @@
+﻿using BenchmarkDotNet.Running;
+using Benchmarks.Yield;
+
+Console.WriteLine("Benchmark starting...");
+
+BenchmarkRunner.Run<YieldBenchmark>();
+
+Console.WriteLine("Benchmark concluded");
---
+++ Benchmarks/Yield/YieldBenchmark.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks.Yield;
+
+[MemoryDiagnoser]
+public class YieldBenchmark
+{
+    [Params(20)]
+    public int N;
+
+    [Benchmark]
+    public void TestWithoutYield()
+    {
+        foreach (var i in GetListWithoutYield(N))
+        {
+            // Do stuff 
+        }
+    }
+
+    [Benchmark]
+    public void TestWithYield()
+    {
+        foreach (var i in GetListWithYield(N))
+        {
+            // Do stuff 
+        }
+    }
+
+    private static IEnumerable<int> GetListWithYield(int max)
+    {
+        for (int i = 0; i < max; i++)
+        {
+            yield return i;
+        }
+    }
+
+    private static IEnumerable<int> GetListWithoutYield(int max)
+    {
+        var list = new List<int>();
+        for (int i = 0; i < max; i++)
+        {
+            list.Add(i);
+        }
+        return list;
+    }
+}
